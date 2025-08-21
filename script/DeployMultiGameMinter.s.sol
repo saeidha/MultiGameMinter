18fc8fd0e7e3bcc3ca593ad5b16a375b8f25af26
saeidha <saeed221373@yahoo.com>
2025-08-22 00:47:43 +0330

    add start

--- script/DeployMultiGameMinter.s.sol
+++ script/DeployMultiGameMinter.s.sol
@@ -6,6 +6,10 @@ import {MultiGameMinter} from "../src/MultiGameMinter.sol";
 
 contract DeployMultiGameMinter is Script {
     function run() external returns (MultiGameMinter) {
-       
+        // Load the private key from the .env file
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        require(deployerPrivateKey != 0, "PRIVATE_KEY not set in .env");
+
+        
     }
 }
